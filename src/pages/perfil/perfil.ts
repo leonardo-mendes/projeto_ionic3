2df46ad69d86b40fa1080c2d3dd0bed6a4271ca3
Leonardo Mendes <leonardocm92@hotmail.com>
2017-11-13 17:14:32 -0200

    Serviço nativo da Camera

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the PerfilPage page.
@@ -12,14 +13,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-perfil',
   templateUrl: 'perfil.html',
+  providers: [
+    Camera
+  ]
 })
 export class PerfilPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  img = "";
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private camera: Camera) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilPage');
+  }
+
+  photo() {
+    // Opções de camera
+    const options: CameraOptions = {
+      quality: 100,
+      // Esse DATA_URL é para gravar a imagem no banco com base64
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then(
+      (imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
